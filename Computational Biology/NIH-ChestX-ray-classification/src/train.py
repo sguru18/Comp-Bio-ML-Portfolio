925b35aedb02102f8201a5587845f3f992392e5d
sguru18 <shguru110@gmail.com>
2026-01-13 12:52:57 -0600

    add auc_roc

--- Computational Biology/NIH-ChestX-ray-classification/src/train.py
+++ Computational Biology/NIH-ChestX-ray-classification/src/train.py
@@ -6,6 +6,9 @@ from src.transforms import train_transform, val_transform
 from pathlib import Path
 import yaml
 from tqdm import tqdm
+import numpy as np
+from sklearn.metrics import roc_auc_score
+from config.config import CLASSES
 
 if __name__ == "__main__":
     device = (
@@ -39,7 +42,7 @@ if __name__ == "__main__":
         val_set, batch_size=32, shuffle=False, num_workers=6
     )
 
-    best_val_loss = 100
+    best_val_auc = 1000000
     for epoch in range(10):
         print(f"-------------EPOCH {epoch}-------------")
 
@@ -60,11 +63,14 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
         avg_train_loss = running_loss / num_batches
         print(f"avg train loss: {avg_train_loss}")
 
         # validation
         model.eval()
+        all_preds = []
+        all_labels = []
         with torch.set_grad_enabled(False):
             num_batches = 0
             running_loss = 0
@@ -72,24 +78,43 @@ if __name__ == "__main__":
                 # transfer to gpu
                 images = batch["image"].to(device)
                 labels = batch["label"].to(device)
+                all_labels.append(labels)
 
-                # model computations...
                 predictions = model(images)
+                all_preds.append(predictions)
                 loss = criterion(predictions, labels)
                 running_loss += loss.item()
                 num_batches += 1
 
             avg_val_loss = running_loss / num_batches
             print(f"avg val loss: {avg_val_loss}")
-            if avg_val_loss < best_val_loss:
+
+            # concatenate predictions and labels and move to cpu to calculate auc-roc
+            all_preds = torch.cat(all_preds, dim=0)
+            all_preds = all_preds.cpu().numpy()
+            all_labels = torch.cat(all_labels, dim=0)
+            all_labels = all_labels.cpu().numpy()
+
+            individual_auc_roc = roc_auc_score(all_labels, all_preds, average=None)
+            for key, value in CLASSES.items():
+                print(f"{key} roc_auc: {individual_auc_roc[value]:.3f}")
+
+            single_auc_roc = roc_auc_score(all_labels, all_preds, average="macro")
+            print(f"MACRO ROC_AUC: {single_auc_roc:.3f}")
+
+            if single_auc_roc < best_val_auc:
+                best_val_auc = single_auc_roc
                 print("saving model to checkpoint.pt")
                 torch.save(
                     {
                         "epoch": epoch,
                         "model_state_dict": model.state_dict(),
                         "optimizer_state_dict": optimizer.state_dict(),
+                        "macro_auc_roc": single_auc_roc,
                         "loss": loss,
-                        "best_val_loss": best_val_loss,
+                        "avg_val_loss": avg_val_loss,
                     },
                     MODELS_DIR / "checkpoint.pt",
                 )
+            else:
+                print("val loss not lower than previous epoch")
