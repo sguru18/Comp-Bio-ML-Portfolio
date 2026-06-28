942966f36dd35608f63380d89529a55d32126746
Srihari <shguru110@gmail.com>
2026-06-28 16:11:26 -0500

    cuda

--- Computational Biology/Cryptic-Site-Detection-GAT/scripts/generate_esmc_embeddings.py
+++ Computational Biology/Cryptic-Site-Detection-GAT/scripts/generate_esmc_embeddings.py
@@ -20,7 +20,13 @@ cif_files_path = (
 embeddings_path = Path(__file__).parent.parent / "data/esmc-embeddings"
 
 # huggingface parameter for device_map only works for cuda multi-gpu
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
 model = AutoModel.from_pretrained("biohub/ESMC-600M").eval().to(device)
 tokenizer = AutoTokenizer.from_pretrained("biohub/ESMC-600M")
 
