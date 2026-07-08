aa7bd5f4a5c9ae4ee63809697bc5a4d89a9bcd54
Srihari <shguru110@gmail.com>
2026-07-08 12:07:27 -0500

    MLP.py

--- Computational Biology/Cryptic-Site-Detection-GAT/src/MLP.py
+++ Computational Biology/Cryptic-Site-Detection-GAT/src/MLP.py
@@ -1,1 +1,19 @@
 import torch.nn as nn
+from MLPEncoder import MLPEncoder
+
+# just takes the output 128 dim from MLPEncoder and turns it into 1 logit
+
+
+class MLP(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.encoder = MLPEncoder()
+        self.head = nn.Linear(128, 1)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.head(x)
+
+        return x
---
+++ Computational Biology/Cryptic-Site-Detection-GAT/src/scripts/trainMLP.py
@@ -0,0 +1,7 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+from MLP import MLP
+from ResidueDataset import ResidueDataset
